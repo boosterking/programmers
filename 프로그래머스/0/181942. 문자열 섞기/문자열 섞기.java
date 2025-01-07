900aa05fcea11fe7c696a143acf27a23daa446d2
boosterking <53156091+boosterking@users.noreply.github.com>
2025-01-07 22:52:21 +0900

    [level 0] Title: 문자열 섞기, Time: 25.66 ms, Memory: 84.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String str1, String str2) {
+        String answer = "";
+        int l = str1.length() + str2.length();
+        
+        for(int i = 0; i < str1.length(); i++) {
+            answer += str1.charAt(i);
+            answer += str2.charAt(i);
+        }
+        
+        
+        return answer;
+    }
+}
