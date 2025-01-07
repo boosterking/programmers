6bfbff3ecf4cc067d8cd0fb7c530b602006f386d
boosterking <53156091+boosterking@users.noreply.github.com>
2025-01-08 00:55:58 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.02 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -1,9 +1,6 @@
 class Solution {
     public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
-        boolean answer = true;
-        boolean a = (x1 || x2) ? true : false;
-        boolean b = (x3 || x4) ? true : false;
-         
-        return answer = (a && b) ? true : false;
+        boolean answer = ((x1 || x2) && (x3 || x4));        
+        return answer;
     }
 }
