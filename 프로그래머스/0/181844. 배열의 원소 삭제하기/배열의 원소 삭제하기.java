3aafd2abeb2620e3eab3d3ec809e5201e67d91f6
boosterking <53156091+boosterking@users.noreply.github.com>
2025-01-12 22:28:25 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.84 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(int[] arr, int[] delete_list) {
+        List<Integer> answer = new ArrayList<>();
+        for(int num : arr) {
+            answer.add(num);
+        }
+        for(int del : delete_list) {
+            answer.remove((Integer)del);
+        }
+        return answer;
+    }
+}
