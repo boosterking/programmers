47dec9d2ee1731cade7bdb6002f7a5c601e97809
boosterking <53156091+boosterking@users.noreply.github.com>
2025-01-13 19:18:55 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.02 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        
+        for(int a : array) {
+            answer += a > height ? 1 : 0;
+        }
+        return answer;
+    }
+}
