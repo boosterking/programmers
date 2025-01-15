017829160e1a0c7f4e8dcc34e15f01243a51d6d0
boosterking <53156091+boosterking@users.noreply.github.com>
2025-01-15 23:24:27 +0900

    [level 0] Title: 숫자 찾기, Time: 10.59 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,10 +1,5 @@
 class Solution {
     public int solution(int num, int k) {
-        String numstr = String.valueOf(num);
-        String kstr = String.valueOf(k);
-        
-        int idx = numstr.indexOf(kstr);
-        
-        return idx == -1 ? -1 : idx + 1;
+        return ("-" + num).indexOf(String.valueOf(k));
     }
 }
