c46da9279ca090ecd16338a2bf1070da66c0d3fc
boosterking <53156091+boosterking@users.noreply.github.com>
2025-01-15 23:12:50 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.05 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(int age) {
+        String answer = String.valueOf(age);
+        return answer.replace("0", "a")
+                        .replace("1", "b")
+                        .replace("2", "c")
+                        .replace("3", "d")
+                        .replace("4", "e")
+                        .replace("5", "f")
+                        .replace("6", "g")
+                        .replace("7", "h")
+                        .replace("8", "i")
+                        .replace("9", "j");
+    }
+}
