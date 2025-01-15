eb13052ef42451528eb6b0bc38386913b63f9f3c
boosterking <53156091+boosterking@users.noreply.github.com>
2025-01-15 23:19:53 +0900

    [level 0] Title: 369게임, Time: 0.03 ms, Memory: 69.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        String orderstr = String.valueOf(order);
+        for(int i = 0; i< orderstr.length(); i++) {
+            if(orderstr.charAt(i) == '3' || orderstr.charAt(i) == '6' || orderstr.charAt(i) == '9') answer++;
+        }
+
+        return answer;
+    }
+}
