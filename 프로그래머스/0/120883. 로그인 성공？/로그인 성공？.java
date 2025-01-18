b2a4f0c9b3a5bffc641e6e06c8003afa1fca2d39
boosterking <53156091+boosterking@users.noreply.github.com>
2025-01-18 18:17:20 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String id = id_pw[0];
+        String pw = id_pw[1];
+        
+        for(String[] user : db) {
+            if (user[0].equals(id)) {
+                return user[1].equals(pw) ? "login" : "wrong pw";
+            }
+        }
+        
+        return "fail";
+    }
+}
