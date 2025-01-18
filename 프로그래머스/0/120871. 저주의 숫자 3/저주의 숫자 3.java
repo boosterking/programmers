e66422931a8bf38f70053ff2625ab7ee56653f7a
boosterking <53156091+boosterking@users.noreply.github.com>
2025-01-18 18:42:29 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.24 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -1,14 +1,12 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int x = 1;
-        
-        while (x <= n) {
+
+        for(int i = 1; i <= n; i++) {
             answer++;
             if(answer % 3 == 0 || String.valueOf(answer).contains("3")) {
-                continue;
+                i--;
             }
-            x++;
         }
         
         return answer;
