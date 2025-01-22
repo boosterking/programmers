e2f8743ccde0e1f216af2faeab9e10db3d4ad32e
boosterking <53156091+boosterking@users.noreply.github.com>
2025-01-22 23:18:05 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 3.11 ms, Memory: 92.2 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,10 +1,7 @@
 class Solution {
     public String solution(String phone_number) {
-        StringBuilder answer = new StringBuilder();
-        for (int i = 0; i < phone_number.length() - 4; i++) {
-            answer.append("*");
-        }
-        answer.append(phone_number.substring(phone_number.length() - 4));
-        return answer.toString();
+        String answer = "*".repeat(phone_number.length() - 4);
+        answer += phone_number.substring(phone_number.length() - 4);
+        return answer;
     }
 }
