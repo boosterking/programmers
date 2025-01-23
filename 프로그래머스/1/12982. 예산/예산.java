951e6678d50641d418a965b8097d038fd68b8f8d
boosterking <53156091+boosterking@users.noreply.github.com>
2025-01-23 21:44:28 +0900

    [level 1] Title: 예산, Time: 0.55 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        int count = 0;
+        
+        for (int amount : d) {
+            if (budget >= amount) {
+                budget -= amount;
+                count++;
+            } else {
+                break;
+            }
+        }
+        
+        return count;
+    }
+}
