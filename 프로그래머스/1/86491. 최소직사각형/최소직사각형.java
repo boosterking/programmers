d36dd5461ba80d5d4cc70ad975012f2af16ced71
boosterking <53156091+boosterking@users.noreply.github.com>
2025-01-23 21:59:52 +0900

    [level 1] Title: 최소직사각형, Time: 1.95 ms, Memory: 96 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        int maxW = 0;
+        int maxH = 0;
+        for(int[] card : sizes) {            
+            maxW = Math.max(maxW, Math.max(card[0], card[1]));
+            maxH = Math.max(maxH, Math.min(card[0], card[1])); 
+        }
+        return maxW * maxH;
+    }
+}
