89343662134beff690fda58d5de8c8c8fc63b033
boosterking <53156091+boosterking@users.noreply.github.com>
2025-01-28 23:37:19 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 100.68 ms, Memory: 96.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> participantMap = new HashMap<>();
+        
+        for(String name : participant) {
+            participantMap.put(name, participantMap.getOrDefault(name, 0) + 1);
+        }
+        
+        for(String name : completion) {
+            participantMap.put(name, participantMap.get(name) - 1);
+        }
+        
+        for(String name : participantMap.keySet()) {
+            if(participantMap.get(name) > 0) {
+                return name;
+            }
+        }
+        
+        return "";
+    }
+}
