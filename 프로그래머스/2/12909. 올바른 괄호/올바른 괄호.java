98cf908097242ce9170926fb5fd476ded24b1dbc
boosterking <53156091+boosterking@users.noreply.github.com>
2025-02-03 23:05:32 +0900

    [level 2] Title: 올바른 괄호, Time: 5.89 ms, Memory: 54.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,16 @@
+class Solution {
+    boolean solution(String s) {
+        int count = 0;
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                count++;
+            } else if (c == ')') {
+                if (count == 0) return false;
+                count--;
+            }
+        }
+        
+        return count == 0;
+    }
+}
