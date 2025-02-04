698d6edaa6a4a9cf389883440b2e503a50c5cace
boosterking <53156091+boosterking@users.noreply.github.com>
2025-02-04 23:47:40 +0900

    [level 2] Title: 귤 고르기, Time: 78.67 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        Map<Integer, Integer> sizeCount = new HashMap<>();
+        
+        for (int size : tangerine) {
+            sizeCount.put(size, sizeCount.getOrDefault(size, 0) + 1);
+        }
+        
+        List<Integer> counts = new ArrayList<>(sizeCount.values());
+        Collections.sort(counts, Collections.reverseOrder());
+        
+        int sum = 0;
+        int types = 0;
+        
+        for (int count : counts) {
+            sum += count;
+            types++;
+            if (sum >= k) break;
+        }
+        
+        return types;
+    }
+}
