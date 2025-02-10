ec9e33aafd9f373b0c7a824423fcf2516e0ada83
boosterking <53156091+boosterking@users.noreply.github.com>
2025-02-10 23:41:54 +0900

    [level 2] Title: 주식가격, Time: 64.72 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,17 +1,24 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
         int n = prices.length;
         int[] answer = new int[n];
-        
+        Stack<Integer> stack = new Stack<>();
+
         for(int i = 0; i < n; i++) {
-            int time = 0;
-            for(int j = i + 1; j < n; j++) {
-                time++;
-                if(prices[j] < prices[i]) break;
+            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int idx = stack.pop();
+                answer[idx] = i - idx;
             }
-            answer[i] = time;
+            stack.push(i);
         }
-        
+
+        while(!stack.isEmpty()) {
+            int idx = stack.pop();
+            answer[idx] = n - idx - 1;
+        }
+
         return answer;
     }
 }
