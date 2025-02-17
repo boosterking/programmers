5a90a6e21a58cd4b345d82f5c024ef500183aca2
boosterking <53156091+boosterking@users.noreply.github.com>
2025-02-17 23:47:23 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 10.46 ms, Memory: 134 MB -BaekjoonHub

---
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -0,0 +1,31 @@
+class Solution {
+    int[] answer = new int[2];
+    
+    public int[] solution(int[][] arr) {
+        compress(arr, 0, 0, arr.length);
+        return answer;
+    }
+    
+    private void compress(int[][] arr, int x, int y, int size) {
+        if(isAllSame(arr, x, y, size)) {
+            answer[arr[x][y]]++;
+            return;
+        }
+        
+        int newSize = size / 2;
+        compress(arr, x, y, newSize);
+        compress(arr, x, y + newSize, newSize);
+        compress(arr, x + newSize, y, newSize);
+        compress(arr, x + newSize, y + newSize, newSize);
+    }
+    
+    private boolean isAllSame(int[][] arr, int x, int y, int size) {
+        int value = arr[x][y];
+        for(int i = x; i < x + size; i++) {
+            for(int j = y; j < y + size; j++) {
+                if(arr[i][j] != value) return false;
+            }
+        }
+        return true;
+    }
+}
