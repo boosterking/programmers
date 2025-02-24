fa94417af3cc40f495517aad06b7d294bd83d71d
boosterking <53156091+boosterking@users.noreply.github.com>
2025-02-24 17:14:04 +0900

    [level 2] Title: 시소 짝꿍, Time: 134.77 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -11,9 +11,9 @@ class Solution {
             answer += map.getOrDefault(w, 0);
             
             map.put(w, map.getOrDefault(w, 0) + 1);
-            map.put(w*4/3, map.getOrDefault(w*4/3, 0) + 1);
-            map.put(w*3/2, map.getOrDefault(w*3/2, 0) + 1);
-            map.put(w*2, map.getOrDefault(w*2, 0) + 1);
+            map.put(w * 4 / 3, map.getOrDefault(w * 4 / 3, 0) + 1);
+            map.put(w * 3 / 2, map.getOrDefault(w * 3 / 2, 0) + 1);
+            map.put(w * 2, map.getOrDefault(w * 2, 0) + 1);
         }
         
         return answer;
