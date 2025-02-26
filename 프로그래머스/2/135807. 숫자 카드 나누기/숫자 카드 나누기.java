b338efaa9cbc8fd35cb082b5bde46d5cfdb91c68
boosterking <53156091+boosterking@users.noreply.github.com>
2025-02-26 23:30:35 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 758.47 ms, Memory: 136 MB -BaekjoonHub

---
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] arrayA, int[] arrayB) {
+        int answer = 0;
+        int num = Math.max(arrayA[0], arrayB[0]);
+        for(int i = 2; i <= num; i++) {
+            if((dividing(arrayA, i) && notDividing(arrayB,i)) || (dividing(arrayB, i) && notDividing(arrayA,i))) {
+                answer = i;
+            }
+        }
+        return answer;
+    }
+    
+    public boolean dividing(int[] arr, int num){
+        for(int n : arr) {
+            if(n % num == 0) continue;
+            else return false;
+        }
+
+        return true;
+    }
+
+    public boolean notDividing(int[] arr, int num){
+        for(int n : arr) {
+            if(n % num != 0) continue;
+            else return false;
+        }
+        return true;
+    }
+}
